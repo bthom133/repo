0acf2f237a62be360086217d3551f65f380f0a20
Brandon K. Thomas <bthom133@students.kennesaw.edu>
2017-03-05 00:13:32 -0500

    This is the second project in the course.

---
+++ cse1311/mini-project2/mini-project2.cpp/mini-project2.cpp.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************************************
+CSE 1311 - C++ Programming for Engineers
+Brandon K. Thomas
+Mini-Project 2: Build a Simple Game Program
+
+Goal(s): The Game is called Twenty-One.
+
+The user rolls dice in an attempt to hit exactly 21. If the user goes over 21, they lose. If they hit 21 exactly, they win. Then they can play the game again if they want.  At first  the user should roll 2 dice. Once their total hits 15 or above, they should only roll one dice. (Use a random number generator to simulate the dice rolls.)
+
+Project II – Build another Game Program
+
+This Game is called Guessing Game.
+
+Write a program to implement the Number Guessing Game. The main method should loop as long   as the player wants to play the game again.  Write a function called PlayGame(). In this  function write the code to play the game. In the game the computer chooses a random       number between 1 and 100, and the player tries to guess the number in as few attempts as  possible. Each time the player enters a guess, the computer tells the player whether the  guess is too high, too low, or right. Once the player guesses the number, that game is    over. Keep track of how many guesses it takes the player to get the number correct.  The  main function should use an array to keep track of how many attempts it takes the player  to guess the random number each time they play the game. When the player decides to exit  the game, print out the results of each game and the average number of guesses it took    this player.
+*******************************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
+
